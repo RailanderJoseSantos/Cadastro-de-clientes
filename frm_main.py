2e96a10b7e3085e107f3317d872acf3af84284ba
RailanderJoseSantos <36487179+RailanderJoseSantos@users.noreply.github.com>
2022-10-23 22:02:12 -0300

    Inserindo frames e tornando a janela responsiva

--- frm_main.py
+++ frm_main.py
@@ -4,6 +4,7 @@ class Aplicacao():
     def __init__(self):
         self.janela = janela
         self.tela()
+        self.frames_tela()
         #necessario looping pra manter janela aberta
         janela.mainloop()
     def tela(self):
@@ -14,5 +15,13 @@ class Aplicacao():
         self.janela.resizable(True, True)
         self.janela.maxsize(width=900, height=700)
         self.janela.minsize(width=400, height=300)
+    def frames_tela(self):
+        self.frame1 = Frame(self.janela,bd=4,bg='#dfe3ee',highlightbackground='#759fe6', highlightthickness=3)
+        #POSICAO DO FRAME NA TELA: esquerda= mais prox de 0, dir.=longe de 0
+        self.frame1.place(relx=0.02,rely=0.02, relwidth=0.96, relheight=0.46)
+
+        self.frame2 = Frame(self.janela,bd=4,bg='#dfe3ee',highlightbackground='#759fe6', highlightthickness=3)
+        #altura começa em 50%: 0.5
+        self.frame2.place(relx=0.02,rely=0.5, relwidth=0.96, relheight=0.46)
 Aplicacao()
 
