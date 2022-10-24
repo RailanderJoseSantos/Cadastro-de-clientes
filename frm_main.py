1de6a54cbc8b6ee33c1d13e3819a44177336df21
RailanderJoseSantos <36487179+RailanderJoseSantos@users.noreply.github.com>
2022-10-23 22:13:39 -0300

    Inserindo botões no frame

--- frm_main.py
+++ frm_main.py
@@ -5,6 +5,7 @@ class Aplicacao():
         self.janela = janela
         self.tela()
         self.frames_tela()
+        self.cria_botoes()
         #necessario looping pra manter janela aberta
         janela.mainloop()
     def tela(self):
@@ -23,5 +24,21 @@ class Aplicacao():
         self.frame2 = Frame(self.janela,bd=4,bg='#dfe3ee',highlightbackground='#759fe6', highlightthickness=3)
         #altura começa em 50%: 0.5
         self.frame2.place(relx=0.02,rely=0.5, relwidth=0.96, relheight=0.46)
+    def cria_botoes(self):
+        """Botão limpar dados"""
+        self.bt_limpar = Button(self.frame1, text="Limpar")
+        self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
+        """Botão buscar dados"""
+        self.bt_buscar = Button(self.frame1, text="Buscar")
+        self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
+        """Botão novo"""
+        self.bt_novo = Button(self.frame1, text="Novo")
+        self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
+        """Botão alterar dados"""
+        self.bt_alterar = Button(self.frame1, text="Alterar")
+        self.bt_alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
+        """Botão apagar dados"""
+        self.bt_apagar = Button(self.frame1, text="Apagar")
+        self.bt_apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
 Aplicacao()
 
