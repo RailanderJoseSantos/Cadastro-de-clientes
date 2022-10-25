15b59ad1a11fc1d7ea543bda2067b7d490a9a574
RailanderJoseSantos <36487179+RailanderJoseSantos@users.noreply.github.com>
2022-10-24 21:43:52 -0300

    Estilizando labels, botoes e caixa de entrada

--- frm_main.py
+++ frm_main.py
@@ -26,44 +26,51 @@ class Aplicacao():
         self.frame2.place(relx=0.02,rely=0.5, relwidth=0.96, relheight=0.46)
     def widgets_frame1(self):
         """Botão limpar dados"""
-        self.bt_limpar = Button(self.frame1, text="Limpar")
+        self.bt_limpar = Button(self.frame1, text="Limpar", bd=2, bg='#107db2',
+                                fg='white', font=('verdana','8', 'bold'))
         self.bt_limpar.place(relx=0.2, rely=0.1, relwidth=0.1, relheight=0.15)
         """Botão buscar dados"""
-        self.bt_buscar = Button(self.frame1, text="Buscar")
+        self.bt_buscar = Button(self.frame1, text="Buscar", bd=2, bg='#107db2',
+                                fg='white', font=('verdana','8', 'bold'))
         self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
         """Botão novo"""
-        self.bt_novo = Button(self.frame1, text="Novo")
+        self.bt_novo = Button(self.frame1, text="Novo", bd=2, bg='#107db2',
+                                fg='white', font=('verdana','8', 'bold'))
         self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
         """Botão alterar dados"""
-        self.bt_alterar = Button(self.frame1, text="Alterar")
+        self.bt_alterar = Button(self.frame1, text="Alterar", bd=2, bg='#107db2',
+                                fg='white', font=('verdana','8', 'bold'))
         self.bt_alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
         """Botão apagar dados"""
-        self.bt_apagar = Button(self.frame1, text="Apagar")
+        self.bt_apagar = Button(self.frame1, text="Apagar", bd=2, bg='#107db2',
+                                fg='white', font=('verdana','8', 'bold'))
         self.bt_apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
 
+
+
         #criacao da label e entrada do codigo
-        self.lb_codigo = Label(self.frame1, text="Código")
+        self.lb_codigo = Label(self.frame1, text="Código",bg='#dfe3ee', fg='#107db2')
         self.lb_codigo.place(relx=0.05, rely=0.05)
         
-        self.codigo_entry = Entry(self.frame1)
+        self.codigo_entry = Entry(self.frame1,bg='lightgray', fg='#000')
         self.codigo_entry.place(relx=0.05, rely=0.15, relwidth=0.08)
 
         #criacao da label e entrada do nome cliente
-        self.lb_nome = Label(self.frame1, text="Nome")
+        self.lb_nome = Label(self.frame1, text="Nome",bg='#dfe3ee', fg='#107db2')
         self.lb_nome.place(relx=0.05, rely=0.35)
         
         self.nome_entry = Entry(self.frame1)
         self.nome_entry.place(relx=0.05, rely=0.45, relwidth=0.8)
 
         #criacao da label e entrada do telefone
-        self.lb_telefone = Label(self.frame1, text="Telefone")
+        self.lb_telefone = Label(self.frame1, text="Telefone",bg='#dfe3ee', fg='#107db2')
         self.lb_telefone.place(relx=0.05, rely=0.6)
         
         self.telefone_entry = Entry(self.frame1)
         self.telefone_entry.place(relx=0.05, rely=0.7, relwidth=0.4)
 
         #criacao da label e entrada da cidade
-        self.lb_cidade = Label(self.frame1, text="Cidade")
+        self.lb_cidade = Label(self.frame1, text="Cidade",bg='#dfe3ee', fg='#107db2')
         self.lb_cidade.place(relx=0.5, rely=0.6)
         self.cidade_entry = Entry(self.frame1)
         self.cidade_entry.place(relx=0.5, rely=0.7, relwidth=0.4)
